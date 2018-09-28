d1f018eb954a3400104f79df149225a670a8af58
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2018-09-28 23:56:09 +0800

    Updated ex6 of learnyounode

--- learnyounode/ex6-module.js
+++ learnyounode/ex6-module.js
@@ -1,18 +1,46 @@
 var fs = require('fs');
 var path = require('path');
 
+// module.exports = function(file, ext, callback) {
+//     fs.readdir(file, (err, list) => {
+//         filteredFiles = [];
+
+//         if (err) {
+//             return callback(err);
+//         }
+    
+//         list.filter((file) => {
+//             let fileExt = path.extname(file);
+//             if (ext.charAt(0) != '.') {
+//                 fileExt = fileExt.slice(1);
+//             }
+//             if (fileExt == ext) {
+//                 console.log(file);
+//                 filteredFiles.push(file);
+//             }
+//         });
+    
+//         callback(null, filteredFiles);
+//     });
+// }
+
 module.exports = function(file, ext, callback) {
     fs.readdir(file, (err, list) => {
         if (err) {
             return callback(err);
         }
-        if (!err) {
-            list.forEach((file) => {
-                if (path.extname(file).slice(1) == ext) {
-                    console.log(file);
-                }
-            })
-        }
+    
+        list = list.filter((file) => {
+            let fileExt = path.extname(file);
+            if (ext.charAt(0) != '.') {
+                fileExt = fileExt.slice(1);
+            }
+            if (fileExt == ext) {
+                console.log(file);
+            }
+            return fileExt == ext;
+        });
+    
         callback(null, list);
     });
 }
